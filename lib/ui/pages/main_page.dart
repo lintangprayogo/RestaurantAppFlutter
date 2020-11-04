00ab39f93d2741a8cd66310349c60da2538ace7f
Lintang Prayogo <linrangprayogo12@gmail.com>
2020-11-04 19:43:04 +0700

    main_page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -6,26 +6,52 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int selectedPage = 0;
+  PageController pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Container(
-            color: Colors.white,
+            color: mainColor,
           ),
           SafeArea(
               child: Container(
             color: "FAFAFC".toColor(),
           )),
-          Container(
-            child: Center(
-              child: Text("BODY TAB"),
+          SafeArea(
+            child: PageView(
+              controller: pageController,
+              onPageChanged: (idx) {
+                setState(() {
+                  selectedPage = idx;
+                });
+              },
+              children: [
+                Center(
+                  child: Text("Home"),
+                ),
+                Center(
+                  child: Text("Order"),
+                ),
+                Center(
+                  child: Text("Profile"),
+                ),
+              ],
             ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: CustomBottomNavBar(),
+            child: CustomBottomNavBar(
+              selectedIndex: selectedPage,
+              onSelected: (idx) {
+                setState(() {
+                  selectedPage = idx;
+                });
+                pageController.jumpToPage(selectedPage);
+              },
+            ),
           )
         ],
       ),
