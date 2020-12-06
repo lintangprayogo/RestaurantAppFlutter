b94920df4f190a53b6eed52ab3abfc0c7c053c8d
Lintang Prayogo <lintangprayogo12@gmail.com>
2020-12-07 00:44:11 +0700

    Update transaction_services.dart

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -1,8 +1,16 @@
 part of 'services.dart';
 
 class TransactionService {
-  static Future<BaseApiResponse<List<Transaction>>> getTransaction() async {
+    static Future<BaseApiResponse<List<Transaction>>> getTransactions() async {
     await Future.delayed(Duration(milliseconds: 3));
     return BaseApiResponse(value: mockTransactions);
+  }
+
+  static Future<BaseApiResponse<Transaction>> submitTransaction(
+      Transaction transaction) async {
+    await Future.delayed(Duration(milliseconds: 2));
+    return BaseApiResponse(
+        value:
+            transaction.copyWith(id: 123, status: TransactionStatus.pending));
   }
 }
